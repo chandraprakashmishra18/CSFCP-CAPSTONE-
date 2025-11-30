b07c8797cf9f5abbb16d96e0518ece6701f7a4e0
chandraprakashmishra18 <prashantmishra44140@gmail.com>
2025-11-30 19:46:45 +0530

    Create script4.sh

---
+++ script4.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+echo "Setting up Study Planner ... "
+mkdir -p Notes Assignments Completed Reminder resources
+touch tasks. txt deadlines.txt
+echo "Setup Complete! Your study planner is ready"
